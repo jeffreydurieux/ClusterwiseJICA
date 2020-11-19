2e0e77ec722fd35fbe4534ab0333175696f37864
Jeffrey Durieux <durieux.jeffrey@gmail.com>
2020-11-19 17:33:00 +0100

    last save

--- Simulate_CJICA.R
+++ Simulate_CJICA.R
@@ -31,7 +31,7 @@ Simulate_CJICA <- function(Nk, Vm, K, Qm, E, M, type = 1, cor = .5){
   # type 2: S %*% Ak
   # type 3: Sk %*% A
   # type 4: is type 1 but with pairwise correlated signals
-  # note type for only when K = 2
+ 
   
   dnames <- c('b')
   
@@ -55,18 +55,74 @@ Simulate_CJICA <- function(Nk, Vm, K, Qm, E, M, type = 1, cor = .5){
       Slist[[k]] <- S
       
       if(type == 4){
-        r <- matrix(c(1,cor,cor,1), nrow = 2)
-        chol <- chol(r)
-        S2 <- matrix(data = NA, nrow = Vm*M, ncol = Qm)
         
-        for(sig in 1:ncol(Slist[[1]])){
-          ss <- cbind(Slist[[1]][,sig],
-                      s <- icasamp(dname = sample(dnames, size = 1),
-                                   query = 'rnd', nsamp = Vm)  )  
-          ss <- ss %*% chol
-          S2[,sig] <- ss[,2]
+        
+        if(K == 2){
+          r <- matrix(c(1,cor,cor,1), nrow = 2)
+          chol <- chol(r)  
+          S2 <- matrix(data = NA, nrow = Vm*M, ncol = Qm)
+          
+          for(sig in 1:ncol(Slist[[1]])){
+            ss <- cbind(Slist[[1]][,sig],
+                        s <- icasamp(dname = sample(dnames, size = 1),
+                                     query = 'rnd', nsamp = Vm*2)  )  
+            ss <- ss %*% chol
+            S2[,sig] <- ss[,2]
+          }
+          Slist[[2]] <- S2  
+        }else if(K == 3){
+          r <- matrix(c(1,cor,cor-.1,
+                   cor,1,cor,
+                   cor-.1, cor, 1),byrow = T, nrow = 3)
+          
+          
+          chol <- chol(r)
+          
+          S2 <- matrix(data = NA, nrow = Vm*M, ncol = Qm)
+          S3 <- matrix(data = NA, nrow = Vm*M, ncol = Qm)
+          for(sig in 1:ncol(Slist[[1]])){
+            ss <- cbind(Slist[[1]][,sig],
+                        icasamp(dname = sample(dnames, size = 1),
+                                     query = 'rnd', nsamp = Vm*2),
+                        icasamp(dname = sample(dnames, size = 1),
+                                query = 'rnd', nsamp = Vm*2))  
+            ss <- ss %*% chol
+            S2[,sig] <- ss[,2]
+            S3[,sig] <- ss[,3]
+          }
+          Slist[[2]] <- S2
+          Slist[[3]] <- S3
+          
+        }else if(K == 4){
+          r <- matrix(c(1,cor,cor-.1, cor-.2,
+                        cor,1, cor,cor-.1,
+                        cor-.1,cor, 1,cor,
+                        cor-.2,cor-.1, cor,1)
+                      ,byrow = T, nrow = 4)
+          
+          chol <- chol(r)
+          
+          S2 <- matrix(data = NA, nrow = Vm*M, ncol = Qm)
+          S3 <- matrix(data = NA, nrow = Vm*M, ncol = Qm)
+          S4 <- matrix(data = NA, nrow = Vm*M, ncol = Qm)
+          for(sig in 1:ncol(Slist[[1]])){
+            ss <- cbind(Slist[[1]][,sig],
+                        icasamp(dname = sample(dnames, size = 1),
+                                     query = 'rnd', nsamp = Vm*2),
+                        icasamp(dname = sample(dnames, size = 1),
+                                query = 'rnd', nsamp = Vm*2),
+                        icasamp(dname = sample(dnames, size = 1),
+                                query = 'rnd', nsamp = Vm*2))  
+            ss <- ss %*% chol
+            S2[,sig] <- ss[,2]
+            S3[,sig] <- ss[,3]
+            S4[,sig] <- ss[,4]
+          }
+          Slist[[2]] <- S2
+          Slist[[3]] <- S3
+          Slist[[4]] <- S4
         }
-        Slist[[2]] <- S2
+        
       }
       
     }  
@@ -76,7 +132,7 @@ Simulate_CJICA <- function(Nk, Vm, K, Qm, E, M, type = 1, cor = .5){
         s <- numeric()
         for(m in 1:M){
           s <- c(s,s <- icasamp(dname = sample(dnames, size = 1),
-                                query = 'rnd', nsamp = Vm)  )  
+                                query = 'rnd', nsamp = Vm*2)  )  
           }
         S[, q] <- s
       }
@@ -118,9 +174,67 @@ Simulate_CJICA <- function(Nk, Vm, K, Qm, E, M, type = 1, cor = .5){
   return(out)  
 }
 
-test1 <- Simulate_CJICA(Nk = 10, Vm = 500,K = 2, 
-                        Qm = 2, E = .1, M = 2, type = 1)
-# str(test1)
+test1 <- Simulate_CJICA(Nk = 10, Vm = 2500,K = 4, 
+                        Qm = 5, E = .1, M = 2, type = 4, cor = .5)
+str(test1)
+
+combn(1:4,2)
+
+mean(
+c(
+modRV(test1$S[[1]], test1$S[[2]]),
+modRV(test1$S[[1]], test1$S[[3]]),
+modRV(test1$S[[1]], test1$S[[4]]),
+modRV(test1$S[[2]], test1$S[[3]]),
+modRV(test1$S[[2]], test1$S[[4]]),
+modRV(test1$S[[3]], test1$S[[4]]) ) )
+
+modRV(test1$S[[1]], test1$S[[2]])
+modRV(test1$S[[1]], test1$S[[3]])
+modRV(test1$S[[1]], test1$S[[4]])
+modRV(test1$S[[2]], test1$S[[3]])
+modRV(test1$S[[2]], test1$S[[4]])
+modRV(test1$S[[3]], test1$S[[4]]) 
+
+
+Tucker(test1$S[[1]], test1$S[[2]]) %>% round(digits = 3)
+Tucker(test1$S[[1]], test1$S[[3]]) %>% round(digits = 3)
+Tucker(test1$S[[1]], test1$S[[4]]) %>% round(digits = 3)
+Tucker(test1$S[[2]], test1$S[[3]]) %>% round(digits = 3)
+Tucker(test1$S[[2]], test1$S[[4]]) %>% round(digits = 3)
+Tucker(test1$S[[3]], test1$S[[4]]) %>% round(digits = 3)
+
+
+
+test1 <- Simulate_CJICA(Nk = 10, Vm = 2500,K = 3, 
+                        Qm = 2, E = .1, M = 2, type = 4)
+str(test1)
+
+combn(1:3,2)
+mean( c(
+modRV(test1$S[[1]], test1$S[[2]]) ,
+modRV(test1$S[[1]], test1$S[[3]]) ,
+modRV(test1$S[[2]], test1$S[[3]]) ))
+
+modRV(test1$S[[1]], test1$S[[2]]) 
+modRV(test1$S[[1]], test1$S[[3]]) 
+modRV(test1$S[[2]], test1$S[[3]]) 
+
+
+test1 <- Simulate_CJICA(Nk = 10, Vm = 2500,K = 2, 
+                        Qm = 2, E = .1, M = 2, type = 4)
+str(test1)
+
+combn(1:2,2)
+
+modRV(test1$S[[1]], test1$S[[2]]) 
+
+
+
+
+Tucker(test1$S[[1]], test1$S[[2]])
+Tucker(test1$S[[2]], test1$S[[3]])
+Tucker(test1$S[[3]], test1$S[[4]])
 # 
 # test2 <- Simulate_CJICA(Nk = 10, Vm = 500,K = 2, 
 #                         Qm = 2, E = .1, M = 2, type = 2)

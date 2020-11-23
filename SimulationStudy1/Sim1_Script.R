58128565d93d56e246628deee18fcda8ac39068d
Jeffrey Durieux <durieux.jeffrey@gmail.com>
2020-11-23 15:28:04 +0100

    last save of sim1 script

--- SimulationStudy1/Sim1_Script.R
+++ SimulationStudy1/Sim1_Script.R
@@ -15,7 +15,22 @@
 # 
 # Fixed: 5000 voxels, 100 random starts
 
+# alice cluster info:
+# will try to run it on the short partition.. Max time = 03:00:00
+# maxjobsubmit is 100 so need to rewrite the code for --array=[1-100]
+
+
+args <- commandArgs(TRUE)
+#args <- as.numeric(args)
+
+splits <- split(1:4860, ceiling(seq_along(1:4860)/49))
+sp <- args[1]
+
+rows <- splits[[sp]]
+
+
 #### correlated clusters
+setwd('/data/durieuxj/CJICACODE')
 library(mclust) #ARI
 library(plotly) # '>' operator
 source('sortX.R')
@@ -80,6 +95,91 @@ perturbation <- function(p, percentage = 0.1){
   return(np)
 }
 
+clusf <- function(nBlocks, nClus) {
+  #simplyfied cluster generation function using an equal probability
+  clus <- GenerateRandomClustering(nBlocks, nClus, rep(c(1 / nClus), nClus))
+  
+  return(clus)
+}
+
+
+GenerateRandomClustering <- function(nElement , nClust , Prob = NULL)
+{
+  ####GenerateRandomClustering = for Random Starts
+  
+  # Author: Tom F. Wilderjans
+  # nElement: number of elements to be clustered
+  # nClust: number of clusters
+  # Prob (1 x nClust): proportion of elements in each cluster
+  
+  # Added by Jeffrey Durieux: default Prob = equal cluster prob
+  # This done to adjust code later on for potential cluster perbutation?
+  
+  if(is.null(Prob))
+  {
+    Prob <- rep(x = (1/nClust) , nClust)
+  }
+  
+  
+  BestClust = NULL
+  ErrorEncountered = F
+  
+  if (!(length(Prob) == nClust))
+  {
+    cat('there should be as much probabilities as clusters')
+    ErrorEncountered = T
+  }
+  
+  if ((abs(sum(Prob) - 1) > .000000001) | (any(Prob < 0)))
+  {
+    cat('probabilities should sum to one (and cannot be negative)')
+    ErrorEncountered = T
+  }
+  
+  if (!(any(nClust == 1:nElement)))
+  {
+    cat("nClus should be a number between 1 and maximal number of datamatrices (length of DataList)")
+    ErrorEncountered = T
+  }
+  
+  if (!(ErrorEncountered))
+  {
+    if (nElement > nClust)
+    {
+      if (nClust == 1)
+      {
+        BestClust = rep(1 , times = nElement)
+      }
+      else
+      {
+        ProbVV = round(Prob * nElement)
+        if (!(sum(ProbVV) == nElement) |
+            (any(ProbVV < 1)))
+          #not enough elements, or empty clusters
+        {
+          ProbVV = AdjustProb(ProbVV , nElement)
+        }
+        
+        tempclus = rep(1:length(ProbVV) , ProbVV)
+        BestClust = tempclus[sample(1:nElement,size = nElement,replace =
+                                      FALSE)]
+      }
+    }
+    else
+    {
+      BestClust = 1:nClust
+    }
+  }
+  
+  if (!(length(unique(BestClust)) == nClust))
+  {
+    BestClust = NULL
+  }
+  
+  return(BestClust)
+}
+
+
 
 Q <- c(2,5,10)
 R <- c(2, 3, 4)
@@ -89,10 +189,19 @@ E <- c(.2, .4, .75)
 rep <- 1:20
 grid <- expand.grid(Q = Q, R = R, N = N, rho = rho, E = E, rep = rep)
 
+grid <- grid[rows,]
 
 for(sim in 1:nrow(grid)){
   
-  set.seed(sim)
+  cat('\n')
+  cat('\n')
+  cat('------------')
+  cat(sim)
+  cat('------------')
+  cat('\n')
+  cat('\n')
+  seed <- as.numeric(rownames(grid))[sim]
+  set.seed(seed)
   
   if(grid[sim, ]$rho == 0){
     type = 1
@@ -178,42 +287,43 @@ for(sim in 1:nrow(grid)){
   }
   
   ### Tucker A ###
-  tuckA <- list()
-  for(tucks in 1:length(simdata$A) ){
-    
-    if(grid[sim,]$R == 2){
-      tuckA[[tucks]] <- c(Tucker(simdata$A[[tucks]], optimal$ica$Mr[[1]]) %>% abs %>% 
-                            apply(MARGIN = 2, max) %>% mean, #i and 1
-                          Tucker(simdata$A[[tucks]], optimal$ica$Mr[[2]]) %>% abs %>% 
-                            apply(MARGIN = 2, max) %>% mean )#i and 1
-    }else if(grid[sim,]$R == 3){
-      tuckA[[tucks]] <- c(Tucker(simdata$A[[tucks]], optimal$ica$Mr[[1]]) %>% abs %>% 
-                            apply(MARGIN = 2, max) %>% mean, #i and 1
-                          Tucker(simdata$A[[tucks]], optimal$ica$Mr[[2]]) %>% abs %>% 
-                            apply(MARGIN = 2, max) %>% mean, #i and 2
-                          Tucker(simdata$A[[tucks]], optimal$ica$Mr[[3]]) %>% abs %>% 
-                            apply(MARGIN = 2, max) %>% mean )  #i and 3
-      
-    }else{
-      tuckA[[tucks]] <- c(Tucker(simdata$A[[tucks]], optimal$ica$Mr[[1]]) %>% abs %>% 
-                            apply(MARGIN = 2, max) %>% mean , #i and 1
-                          Tucker(simdata$A[[tucks]], optimal$ica$Mr[[2]]) %>% abs %>% 
-                            apply(MARGIN = 2, max) %>% mean , #i and 2
-                          Tucker(simdata$A[[tucks]], optimal$ica$Mr[[3]]) %>% abs %>% 
-                            apply(MARGIN = 2, max) %>% mean , #i and 3
-                          Tucker(simdata$A[[tucks]], optimal$ica$Mr[[4]]) %>% abs %>% 
-                            apply(MARGIN = 2, max) %>% mean) #i and 4
-    }
-    
-  }
+  #note: will cause error when no perfect P recovery. solve later after ALICE SIM
+  # tuckA <- list()
+  # for(tucks in 1:length(simdata$A) ){
+  #   
+  #   if(grid[sim,]$R == 2){
+  #     tuckA[[tucks]] <- c(Tucker(simdata$A[[tucks]], optimal$ica$Mr[[1]]) %>% abs %>% 
+  #                           apply(MARGIN = 2, max) %>% mean, #i and 1
+  #                         Tucker(simdata$A[[tucks]], optimal$ica$Mr[[2]]) %>% abs %>% 
+  #                           apply(MARGIN = 2, max) %>% mean )#i and 1
+  #   }else if(grid[sim,]$R == 3){
+  #     tuckA[[tucks]] <- c(Tucker(simdata$A[[tucks]], optimal$ica$Mr[[1]]) %>% abs %>% 
+  #                           apply(MARGIN = 2, max) %>% mean, #i and 1
+  #                         Tucker(simdata$A[[tucks]], optimal$ica$Mr[[2]]) %>% abs %>% 
+  #                           apply(MARGIN = 2, max) %>% mean, #i and 2
+  #                         Tucker(simdata$A[[tucks]], optimal$ica$Mr[[3]]) %>% abs %>% 
+  #                           apply(MARGIN = 2, max) %>% mean )  #i and 3
+  #     
+  #   }else{
+  #     tuckA[[tucks]] <- c(Tucker(simdata$A[[tucks]], optimal$ica$Mr[[1]]) %>% abs %>% 
+  #                           apply(MARGIN = 2, max) %>% mean , #i and 1
+  #                         Tucker(simdata$A[[tucks]], optimal$ica$Mr[[2]]) %>% abs %>% 
+  #                           apply(MARGIN = 2, max) %>% mean , #i and 2
+  #                         Tucker(simdata$A[[tucks]], optimal$ica$Mr[[3]]) %>% abs %>% 
+  #                           apply(MARGIN = 2, max) %>% mean , #i and 3
+  #                         Tucker(simdata$A[[tucks]], optimal$ica$Mr[[4]]) %>% abs %>% 
+  #                           apply(MARGIN = 2, max) %>% mean) #i and 4
+  #   }
+  #   
+  # }
   
   #### output list object #####
   output <- list()
   output$id <- grid[sim,]
-  output$seed <- sim
+  output$seed <- seed
   output$ari <- ari
   output$S <- tuckS
-  output$A <- tuckA
+  output$Araw <- optimal$ica$Mr
   output$time <- time
   output$optimal <- optimal
   output$loss100 <- loss100
@@ -221,7 +331,9 @@ for(sim in 1:nrow(grid)){
   output$cjicaP <- cjicaP[[1]]
   output$cjicaperbs <- cjicaperbs
   
-  ext <- paste('CJICA_sim1_',sim, '.Rdata',sep = '')
+  
+  ext <- paste('/home/durieuxj/data/CJICA_results/Sim1/',
+               'CJICA_sim1_',rows[sim], '.Rdata',sep = '')
   save(output,file = ext)
   
 }

62fd5e42e992af856b0c1de2c2eca598a87cd461
Jeffrey Durieux <durieux.jeffrey@gmail.com>
2020-12-01 17:49:11 +0100

    last save

--- SimulationStudy1/Gather_results_Sim1.R
+++ SimulationStudy1/Gather_results_Sim1.R
@@ -6,7 +6,8 @@
 library(gtools)
 library(doBy)
 library(stringr)
-
+library(mclust)
+source('~/Repositories/PhDthesis/P4/Simulate_CJICA.R')
 setwd('/Volumes/LaCie/MyData/CJICA/Sim1/Sim1/')
 
 
@@ -44,7 +45,12 @@ LOSStrueP <- numeric()
 LOSSperb <- numeric()
 LOSSperbrange <- list()
 LOSS100 <- list()
+ARI_trueP <- numeric()
+ARI_perbP <- numeric()
 
+tmp <- proc.time()
+
+# timing of this loop was: 1069 seconds/ 18 minutes
 for(i in 1:nrow(grid)){
   cat('Extracting results from grid number: ',i,'\n')
   load(file = files[i])
@@ -61,8 +67,53 @@ for(i in 1:nrow(grid)){
   LOSSperbrange[[i]] <- range( sapply(seq_along(output$cjicaperbs), function(lam) 
     tail(output$cjicaperbs[[lam]]$lossiter, n = 1) ) )
   LOSS100[[i]] <- output$loss100
+  
+  seed <- as.numeric(rownames(grid))[i]
+  set.seed(seed)
+  
+  if(grid[i, ]$rho == 0){
+    type = 1
+  }else{
+    type = 4
+  }
+  
+  ##### simulate #####
+  simdata <- Simulate_CJICA(Nk = grid[i,]$N, 
+                            Vm = 2500,
+                            K = grid[i, ]$R,
+                            Qm = grid[i, ]$Q,
+                            E = grid[i, ]$E,
+                            M = 2,
+                            cor = grid[i, ]$rho,
+                            type = type 
+  )
+  
+  ARI_trueP[i] <- adjustedRandIndex(simdata$P, output$cjicaP$p)
+  
+  id_perb <- which.min (sapply(seq_along(output$cjicaperbs), 
+         function(lam) tail(output$cjicaperbs[[lam]]$lossiter, n = 1)) )
+  ARI_perbP[i] <- adjustedRandIndex(simdata$P,output$cjicaperbs[[id_perb]]$p)
+  
+  
 }
 
+gathering_time <- proc.time() - tmp
+
+ARI <- numeric()
+TIME <- numeric()
+TuckS <- numeric()
+LOSSrandom <- numeric()
+LOSStrueP <- numeric()
+LOSSperb <- numeric()
+LOSSperbrange <- list()
+LOSS100 <- list()
+ARI_trueP <- numeric()
+ARI_perbP <- numeric()
+
+data <- cbind(grid, ARI, ARI_trueP, ARI_perbP, TuckS, LOSSrandom, LOSStrueP, LOSSperb)
+save(data, file = '../Sim1_results_df.Rdata')
+save(LOSS100, file = '../Sim1_results_LOSS100_list.Rdata')
+save(LOSSperbrange, file = '../Sim1_results_LOSSperbrange_list.Rdata')
 
 
 #### graph of loss 100 summary ####
--- SimulationStudy1/Sim1_summarystatistics.R
+++ SimulationStudy1/Sim1_summarystatistics.R
@@ -4,17 +4,77 @@
 
 # What: summary statistics of clusterwise JICA simulation 1
 
-res <- cbind(grid, ARI = ARI)
+load('/Volumes/LaCie/MyData/CJICA/Sim1/Sim1_results_df.Rdata')
+res <- data
 
+########### SUMMARY STATISTICS ##########
+
+######## ARI ########
 summary_by(data = res, formula = ARI ~ .)
 summary_by(data = res, formula = ARI ~ Q)
 summary_by(data = res, formula = ARI ~ R)
 summary_by(data = res, formula = ARI ~ rho)
 summary_by(data = res, formula = ARI ~ E)
+summary_by(data = res, formula = ARI ~ N)
 
+summary_by(data = res, formula = ARI ~ N:Q)
+summary_by(data = res, formula = ARI ~ rho:Q)
+summary_by(data = res, formula = ARI ~ E:N)
+
+####### ARI true P #######
+summary_by(data = res, formula = ARI_trueP ~ .)
+summary_by(data = res, formula = ARI_trueP ~ Q)
+summary_by(data = res, formula = ARI_trueP ~ R)
+summary_by(data = res, formula = ARI_trueP ~ rho)
+summary_by(data = res, formula = ARI_trueP ~ E)
+summary_by(data = res, formula = ARI_trueP ~ N)
+
+###################PLOTS#################
+
+######## check aris of random100 vs trueP and perb_P #####
+par(mfrow=c(1,1))
+range(res$ARI_trueP)
+plot(res$ARI, res$ARI_trueP, ylim = c(0,1), asp=T)
+abline(0,1)
+
+range(res$ARI_perbP)
+plot(res$ARI, res$ARI_perbP, ylim = c(0,1))
+abline(0,1)
+
+######## check loss of random100 vs trueP and perb_P #####
+
+plot(res$LOSSrandom,res$LOSStrueP)
+
+
+par(mfrow= c(1,2))
+LossTruep_min_Random <-  res$LOSSrandom - res$LOSStrueP 
+range(LossTruep_min_Random)
+plot(LossTruep_min_Random, ylim= range(LossTruep_min_Random))
+
+LossTrueP_min_perb <- res$LOSSperb - res$LOSStrueP
+plot(LossTrueP_min_perb, ylim = range(LossTruep_min_Random))
+
+### interesting ids ####
+ids <- which(LTR < 0)
+res[ids,c(1,2,3,4,5, 7,8, 11,12)]
+
+
+for(p in 1:100){
+  cat('p index ', p, '\n')
+  print(res$LOSStrueP[p])
+  print(res$LOSSrandom[p])
+  
+}
+p = 5
+res$LOSStrueP[p]
+res$LOSSrandom[p]
+
+
+
+######### ANOVA ##########
 library(ez)
 
-dat <- data.frame(id = 1:4639, res)
+dat <- data.frame(id = 1:4860, res)
 
 ari_anova_res <- ezANOVA( data = dat , ARI , wid = id, within = NULL , within_full = NULL , within_covariates = NULL , between = .('Q','R','N','rho','E') , between_covariates = NULL , observed = NULL , diff = NULL , reverse_diff = FALSE , type = 3 , white.adjust = FALSE , detailed = FALSE , return_aov = TRUE)
 
@@ -23,6 +83,22 @@ ari_anova_res$ANOVA$ges %>% round(digits = 3)
 cbind(ari_anova_res$ANOVA,ef = ari_anova_res$ANOVA$ges %>% round(digits = 3))
 
 
+##### ANOVA with ratio variable #####
+ratio <- res$N/res$Q
+res <- cbind(res,ratio)
+
+dat <- data.frame(id = 1:4860, res)
+ari_anova_res <- ezANOVA( data = dat , ARI , wid = id, within = NULL , within_full = NULL , within_covariates = NULL , between = .('ratio','E') , between_covariates = NULL , observed = NULL , diff = NULL , reverse_diff = FALSE , type = 3 , white.adjust = FALSE , detailed = FALSE , return_aov = TRUE)
+
+print(ari_anova_res)
+ari_anova_res$ANOVA$ges %>% round(digits = 3)
+cbind(ari_anova_res$ANOVA,ef = ari_anova_res$ANOVA$ges %>% round(digits = 3))
+
+
+
+
+
+##########
 summary <- summary_by(data = res, formula = ARI ~ Q:N:E)
 ratio <- summary$N/summary$Q
 cbind(summary,ratio)

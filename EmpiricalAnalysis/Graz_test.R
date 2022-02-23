eb006ff1f9858efea5f4331e8344c62269bdd6a0
jeffreydurieux <durieux.jeffrey@gmail.com>
2022-02-23 10:23:42 +0100

    update

--- EmpiricalAnalysis/Graz_test.R
+++ EmpiricalAnalysis/Graz_test.R
@@ -15,4 +15,15 @@ source('Avoid_nc_N.R')
 source('Simulate_CJICA.R')
 source('ClusterwiseJICA.R')
 
-#test
+
+load('~/Documents/Grazdata_CJICA/AlzheimerfMRI_extra_aangevuld.RData')
+
+#### Take VBM4mm and ALFF4mm --> Christiane's best option
+
+dim(ALFF4mm)  # 25750
+dim(VBM4mm)   # 59049
+
+X <- cbind(VBM4mm, ALFF4mm) # 250 x 59049 | 25750
+
+
+
